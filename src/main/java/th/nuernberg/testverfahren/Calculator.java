303cf210b85cddf23709db2256a5641d34532bc4
Sebastian Feltl <feltlse90427@th-nuernberg.de>
2025-07-04 10:09:03 +0200

    Aufgabe 4 update calc

--- src/main/java/th/nuernberg/testverfahren/Calculator.java
+++ src/main/java/th/nuernberg/testverfahren/Calculator.java
@@ -1,11 +1,11 @@
 package th.nuernberg.testverfahren;
 
-public class Calculator {
-    public int add(int a, int b) {
+public class final Calculator {
+    public int add(final int a, final int b) {
         return a + b;
     }
 
-    public double divide(int a, int b) {
+    public double divide(final int a, final int b) {
         if (b == 0) {
             throw new IllegalArgumentException("Division by zero!");
         }
